b5eb0c911e2da1a105a286018f2c92ba20b3009f
S13G <ayflix0@gmail.com>
2022-09-08 17:03:19 +0100

    populated place database with airport

--- external.py
+++ external.py
@@ -25,8 +25,10 @@ result = json.loads(json_obj)
 
 for i in range(50):
     data = result["data"][i]
-    if data["iata_country_code"] in Country.objects.all().values_list("code", flat=True):
-        Place.objects.create(name=data["name"], country=data["iata_country_code"],
+    values_list = Country.objects.all().values_list("code", flat=True)
+    iata_country = Country.objects.get(code=data["iata_country_code"])
+    if data["iata_country_code"] in values_list:
+        Place.objects.create(name=data["name"], country=iata_country,
                              category="Airport", code=data["iata_code"], longitude=data["longitude"],
                              latitude=data["latitude"])
     else:
