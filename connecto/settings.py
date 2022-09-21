7047fe5cf141df5fb2f4707ded856ad17e4c6548
s13g <ayflix0@gmail.com>
2022-09-21 23:39:59 +0100

    fixes

--- connecto/settings.py
+++ connecto/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'connectotransfers.herokuapp.com']
 
 
 # Application definition
