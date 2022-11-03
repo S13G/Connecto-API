98acf6fd0ee0c8f3a13fc7a5c95757f80c8e5ae5
s13g <ayflix0@gmail.com>
2022-11-03 15:00:47 +0100

    added icons to admin page and did some fixes

--- connecto/settings.py
+++ connecto/settings.py
@@ -97,7 +97,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'booking.middleware.AuthorizedOriginMiddleware'
+    'booking.middleware.AuthorizedOriginMiddleware'
 ]
 
 # INTERNAL_IPS = [
