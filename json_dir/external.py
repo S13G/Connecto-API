0b8b1b59263a9c9424e2ce20dec7a74208b9157b
s13g <ayflix0@gmail.com>
2023-01-13 13:16:16 +0100

    deployment and db pop done

--- json_dir/external.py
+++ json_dir/external.py
@@ -3,7 +3,7 @@ import json
 import requests
 from decouple import config
 
-from booking.models import Place, Country
+from booking.models import Country, Place
 
 url = "https://api.duffel.com/air/airports"
 api_key = config('API_KEY')
@@ -25,11 +25,13 @@ result = json.loads(json_obj)
 
 for i in range(50):
     data = result["data"][i]
-    values_list = Country.objects.all().values_list("code", flat=True)
-    iata_country = Country.objects.get(code=data["iata_country_code"])
-    if data["iata_country_code"] in values_list:
-        Place.objects.create(name=data["name"], country=iata_country,
-                             category="Airport", code=data["iata_code"], longitude=data["longitude"],
-                             latitude=data["latitude"])
-    else:
-        pass
+    # values_list = Country.objects.all().values_list("code", flat=True)
+    iata_code = data["iata_country_code"]
+    # iata_country = Country.objects.get(code=data["iata_country_code"])
+    country, created = Country.objects.get_or_create(code=iata_code)
+    # if data["iata_country_code"] in values_list:
+    Place.objects.create(name=data["name"], country=country,
+                         category="Airport", code=data["iata_code"], longitude=data["longitude"],
+                         latitude=data["latitude"])
+else:
+    pass
