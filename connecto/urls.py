4d56958bd26150d733ee216f099a1a5868a66701
s13g <ayflix0@gmail.com>
2023-01-13 10:17:18 +0100

    deployment in progress

--- connecto/urls.py
+++ connecto/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
@@ -40,3 +42,6 @@ urlpatterns = [
     path('api/', include('booking.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
