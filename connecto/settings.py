195f5a1a16d333df1676f6c64d203790a8c5e098
s13g <ayflix0@gmail.com>
2023-01-13 11:16:07 +0100

    update deployment

--- connecto/settings.py
+++ connecto/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['connecto.cleverapps.io', '127.0.0.1']
+ALLOWED_HOSTS = ['connecto.cleverapps.io', '127.0.0.1', '10.2.194.209']
 
 # Application definition
 
